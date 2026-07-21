774ba213ca5a3bec4cb9a693ce386c2383693080
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2026-07-21 21:32:17 +0700

    chore(database): add db init script

---
+++ init-postgres/init.sql
@@ -0,0 +1,6 @@
+-- Tạo các database riêng biệt cho từng service
+CREATE DATABASE auth_db;
+CREATE DATABASE core_db;
+CREATE DATABASE booking_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE notification_db;
